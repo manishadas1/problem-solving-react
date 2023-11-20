24aa639b484c31474649f8fd7827c49157b7a638
manishadas1 <manishadas.akrabhi@gmail.com>
2023-11-20 21:02:34 +0530

    search is completed

--- src/components/Stock.js
+++ src/components/Stock.js
@@ -1,60 +1,98 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Papa from 'papaparse';
+import Fuse from 'fuse.js';
 
 const InstrumentsAPI = 'https://prototype.sbulltech.com/api/v2/instruments';
 
 const StocksPage = () => {
-    const [stocks, setStocks] = useState([]);
+  const [stocks, setStocks] = useState([]);
+  const [search, setSearch] = useState('');
+  const [searchRes, setSearchRes] = useState([]);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        
+        const response = await axios.get(InstrumentsAPI);
+        const csvData = response.data;
 
-                const response = await axios.get(InstrumentsAPI);
-                const csvData = response.data;
+        
+        Papa.parse(csvData, {
+          header: true,
+          dynamicTyping: true,
+          complete: (result) => {
+            setStocks(result.data);
+            setSearchRes(result.data);
+          },
+        });
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
+    fetchData();
+  }, []); 
 
-                Papa.parse(csvData, {
-                    header: true,
-                    dynamicTyping: true,
-                    complete: (result) => {
-                        setStocks(result.data);
-                    },
-                });
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
+  useEffect(() => {
 
-        fetchData();
-    }, []);
+    if (search.trim() === '') {
+      setSearchRes(stocks);
+      return;
+    }
 
-    return (
-        <div>
-            <h1>Stocks Page</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Symbol</th>
-                        <th>Name</th>
-                        <th>Sector</th>
-                        <th>Validtill</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {stocks.map((stock) => (
-                        <tr key={stock.Symbol}>
-                            <td>{stock.Symbol}</td>
-                            <td>{stock.Name}</td>
-                            <td>{stock.Sector}</td>
-                            <td>{stock.Validtill}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
-    );
+    const options = {
+      keys: ['Symbol', 'Name' , 'Sector' , 'Validtill'],
+      threshold: 0.5, 
+      match: 'any',
+    };
+
+    const fuse = new Fuse(stocks, options);
+    const results = fuse.search(search);
+    setSearchRes(results.map((result) => result.item));
+  }, [search, stocks]);
+
+  return (
+    <div>
+      <h1>Stocks Page</h1>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Symbol</th>
+            <th>Name</th>
+            <th>Sector</th>
+            <th>Validtill</th>
+          </tr>
+        </thead>
+        <tbody>
+          {searchRes.map((stock) => (
+            <tr key={stock.Symbol}>
+              <td>{stock.Symbol}</td>
+              <td>{stock.Name}</td>
+              <td>{stock.Sector}</td>
+              <td>{stock.Validtill}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default StocksPage;
+
+
+
+
+
+
+
+
+
+
